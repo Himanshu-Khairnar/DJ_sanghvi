241e57a46de79a3852f4dce1f436b22a5377e183
Mohit <mohitkhairnar28@gmail.com>
2025-09-21 17:50:32 +0530

    Revert "Made changes in LL Operations"
    This reverts commit 39c337fac870e172daa17f299043dda54b03fdcb.

--- Dsa/Experiment3/LLOperaionns.c
+++ Dsa/Experiment3/LLOperaionns.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Node structure
 struct Node {
     int data;
     struct Node* next;
 };
 
+// Function to create new node
 struct Node* createNode(int data) {
     struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
     newNode->data = data;
@@ -13,6 +15,7 @@ struct Node* createNode(int data) {
     return newNode;
 }
 
+// Insert node at end
 void insertEnd(struct Node** head, int data) {
     struct Node* newNode = createNode(data);
     if (*head == NULL) {
@@ -25,6 +28,7 @@ void insertEnd(struct Node** head, int data) {
     temp->next = newNode;
 }
 
+// Print linked list
 void printList(struct Node* head) {
     struct Node* temp = head;
     while (temp != NULL) {
@@ -34,6 +38,7 @@ void printList(struct Node* head) {
     printf("\n");
 }
 
+// ---------- Problem 1: Find Middle Node ----------
 struct Node* findMiddle(struct Node* head) {
     struct Node* slow = head;
     struct Node* fast = head;
@@ -41,9 +46,10 @@ struct Node* findMiddle(struct Node* head) {
         slow = slow->next;
         fast = fast->next->next;
     }
-    return slow;
+    return slow;  // slow will be middle (second middle if even length)
 }
 
+// ---------- Problem 2: Segregate Even and Odd ----------
 struct Node* segregateEvenOdd(struct Node* head) {
     struct Node *evenStart = NULL, *evenEnd = NULL;
     struct Node *oddStart = NULL, *oddEnd = NULL;
@@ -51,14 +57,14 @@ struct Node* segregateEvenOdd(struct Node* head) {
 
     while (curr != NULL) {
         int val = curr->data;
-        if (val % 2 == 0) {
+        if (val % 2 == 0) { // Even node
             if (evenStart == NULL) {
                 evenStart = evenEnd = curr;
             } else {
                 evenEnd->next = curr;
                 evenEnd = evenEnd->next;
             }
-        } else { 
+        } else { // Odd node
             if (oddStart == NULL) {
                 oddStart = oddEnd = curr;
             } else {
@@ -69,18 +75,22 @@ struct Node* segregateEvenOdd(struct Node* head) {
         curr = curr->next;
     }
 
+    // If no even or no odd nodes
     if (evenStart == NULL || oddStart == NULL)
         return head;
 
+    // Connect even list to odd list
     evenEnd->next = oddStart;
     oddEnd->next = NULL;
 
     return evenStart;
 }
 
+// ---------- Main ----------
 int main() {
     struct Node* head = NULL;
 
+    // Creating linked list: 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7
     insertEnd(&head, 1);
     insertEnd(&head, 2);
     insertEnd(&head, 3);
@@ -92,9 +102,11 @@ int main() {
     printf("Original List: ");
     printList(head);
 
+    // Problem 1: Find middle
     struct Node* middle = findMiddle(head);
     printf("Middle Node: %d\n", middle->data);
 
+    // Problem 2: Segregate even and odd
     head = segregateEvenOdd(head);
     printf("Modified List (Even before Odd): ");
     printList(head);
